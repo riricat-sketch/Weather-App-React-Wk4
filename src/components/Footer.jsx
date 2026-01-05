4e7f02d3fbe2cee9197cf956f539ec2cafb674c0
riricat-sketch <ri.audoire@octopusev.com>
2026-01-05 10:30:52 +0000

    added link to Netlifly and SheCodes

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,15 +1,32 @@
 export default function Footer() {
   return (
-    <footer>
+    <footer className="footer">
       <p>
-        View this project on{" "}
+        This project was coded by{" "}
+        <a
+          href="https://www.shecodes.io/graduates/158285-briana-audoire"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Ri Audoire
+        </a>{" "}
+        and is open-sourced on{" "}
         <a
           href="https://github.com/riricat-sketch/Weather-App-React-Wk4"
           target="_blank"
           rel="noopener noreferrer"
         >
           GitHub
+        </a>{" "}
+        and hosted on{" "}
+        <a
+          href="https://app.netlify.com/projects/weatherreactri/overview"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Netlify
         </a>
+        .
       </p>
     </footer>
   );
